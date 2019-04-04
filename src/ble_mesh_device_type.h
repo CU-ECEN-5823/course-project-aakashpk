315088d589af78fbcb4908bf418c92064a227d29
aakash <Aakash.Kumar@colorado.edu>
2019-04-04 11:47:21 -0600

    Initial Commit for project, just copied over assignment 10. Code needs to be cleaned up.

--- src/ble_mesh_device_type.h
+++ src/ble_mesh_device_type.h
@@ -14,7 +14,7 @@
  * Set to 1 to build an on/off publisher client model for Assignment 10
  * Set to 0 to build an on/off subscriber server model for Assignment 10
  */
-#define DEVICE_IS_ONOFF_PUBLISHER			1
+#define DEVICE_IS_ONOFF_PUBLISHER			0
 
 #if DEVICE_IS_ONOFF_PUBLISHER
 #define DEVICE_USES_BLE_MESH_CLIENT_MODEL 	1
