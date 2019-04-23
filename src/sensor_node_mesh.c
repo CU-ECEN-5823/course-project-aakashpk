16d74951385ba971a184e531e74a6435459feb5a
aakash <Aakash.Kumar@colorado.edu>
2019-04-23 03:09:59 -0600

    Cleaned up functions and added return checks

--- src/sensor_node_mesh.c
+++ src/sensor_node_mesh.c
@@ -18,11 +18,7 @@ void lpn_init(void)
 {
   uint16 res;
   // Initialize LPN functionality.
-  res = gecko_cmd_mesh_lpn_init()->result;
-  if (res) {
-    LOG_INFO("LPN init failed (0x%x)", res);
-    return;
-  }
+ BTSTACK_CHECK_RESPONSE(gecko_cmd_mesh_lpn_init());
 
   /*
    * param 1 Minimum queue length the friend must support.
@@ -38,19 +34,13 @@ void lpn_init(void)
    * longer periods, at the expense of increased latency for receiving
    * messages.Note that the given value is rounded up to the nearest 100ms
    */
-  res = gecko_cmd_mesh_lpn_configure(2, 5 * 1000)->result;
-  if (res) {
-    LOG_INFO("LPN conf failed (0x%x)", res);
-    return;
-  }
+  BTSTACK_CHECK_RESPONSE(gecko_cmd_mesh_lpn_configure(2, 5 * 1000));
+
 
   LOG_INFO("trying to find friend...");
-  res = gecko_cmd_mesh_lpn_establish_friendship(0)->result;
+  BTSTACK_CHECK_RESPONSE(gecko_cmd_mesh_lpn_establish_friendship(0));
 
-  if (res != 0) {
-    LOG_INFO("Friend failed with code %x\r\n", res);
-  }
-  else lpn_active = 1;
+  lpn_active = 1;
 }
 
 /***************************************************************************//**
@@ -64,22 +54,19 @@ void lpn_deinit(void)
     return; // lpn feature is currently inactive
   }
 
-  result = gecko_cmd_hardware_set_soft_timer(0, // cancel friend finding timer
+  BTSTACK_CHECK_RESPONSE(gecko_cmd_hardware_set_soft_timer(0, // cancel friend finding timer
                                              TIMER_ID_FRIEND_FIND,
-                                             1)->result;
+                                             1));
 
   // Terminate friendship if exist
-  result = gecko_cmd_mesh_lpn_terminate_friendship()->result;
-  if (result) {
-    LOG_INFO("Friendship termination failed (0x%x)", result);
-  }
+  BTSTACK_CHECK_RESPONSE(gecko_cmd_mesh_lpn_terminate_friendship());
+
+  LOG_INFO("Deinitialize LPN");
   // turn off lpn feature
-  result = gecko_cmd_mesh_lpn_deinit()->result;
-  if (result) {
-    LOG_INFO("LPN deinit failed (0x%x)", result);
-  }
+  BTSTACK_CHECK_RESPONSE(gecko_cmd_mesh_lpn_deinit());
+
   lpn_active = 0;
-  LOG_INFO("LPN deinitialized");
+
 }
 
 /**
@@ -107,8 +94,8 @@ void send_button_state(uint8_t state)
 
 	// increment transaction ID for each request
 		trid++;
-		//mesh_lib_generic_client_publish
-	resp = gecko_cmd_mesh_generic_client_publish(
+
+	BTSTACK_CHECK_RESPONSE(gecko_cmd_mesh_generic_client_publish(
 			MESH_GENERIC_ON_OFF_CLIENT_MODEL_ID,
 			0, // element index of primary element is 0
 			trid,
@@ -118,37 +105,23 @@ void send_button_state(uint8_t state)
 			mesh_generic_request_on_off,     // type
 			1,     // param len
 			&req.on_off     /// parameters data
-	)->result;
+	));
 
-	if (resp) {
-		LOG_INFO("gecko_cmd_mesh_generic_client_publish failed,code %x\r\n", resp);
-	} else {
-		LOG_INFO("On/off request sent, trx id = %u", trid);
-	}
 }
 
-/***************************************************************************//**
- * This function publishes one light CTL request to change the temperature level
- * of light(s) in the group. Global variable temperature_level holds the latest
- * desired light temperature level.
- * The CTL request also send lightness_level which holds the latest desired light
- * lightness level and Delta UV which is hardcoded to 0 for this application.
- *
- * param[in] retrans  Indicates if this is the first request or a retransmission,
- *                    possible values are 0 = first request, 1 = retransmission.
- ******************************************************************************/
-void send_sensor_data(uint16_t lightness_level,
-		uint16_t temperature_level,int16_t DELTA_UV,int retrans)
+
+void send_lighting_lightness(uint16_t level,
+		uint8_t kind,
+		uint8_t flag,
+		uint16_t transition,
+		uint16_t delay,
+		int retrans)
 {
-  uint16 resp1 = 1;
-  uint16 delay;
   struct mesh_generic_request req;
 
-  req.kind = mesh_lighting_request_lightness_actual;
-//  req.kind = mesh_lighting_request_ctl;
-  req.ctl.lightness = lightness_level;
-//  req.ctl.temperature = temperature_level;
-//  req.ctl.deltauv = DELTA_UV; //hardcoded delta uv
+//  mesh_lighting_request_lightness_actual;
+  req.kind = kind;
+  req.ctl.lightness = level;
 
   // increment transaction ID for each request, unless it's a retransmission
   if (retrans == 0)
@@ -158,22 +131,26 @@ void send_sensor_data(uint16_t lightness_level,
 
   delay = 0;
 
-  resp1 = mesh_lib_generic_client_publish(
+  MESH_CHECK_RESPONSE(mesh_lib_generic_client_publish(
 		  MESH_LIGHTING_LIGHTNESS_CLIENT_MODEL_ID,
     0,
     trid,
     &req,
-    0,     // transition
+	transition,     // transition
     delay,
-    0     // flags
-    );
+	flag     // flags
+    ));
 
-  if (resp1) {
-    LOG_ERROR("gecko_cmd_mesh_generic_client_publish failed,code %x", resp1);
-  } else {
-    LOG_INFO("Sensor Data sent, trid = %u, delay = %d", trid, delay);
-  }
 }
+
+
+
+
+
+
+
+
+
 
 void send_sensor_data_ctl(uint16_t lightness_level,
 		uint16_t temperature_level,int16_t DELTA_UV,int retrans)
--- src/sensor_node_mesh.h
+++ src/sensor_node_mesh.h
@@ -15,10 +15,20 @@
 void send_button_state(uint8_t state);
 void sensor_node_init(void);
 
-void send_sensor_data(uint16_t lightness_level,
-					  uint16_t temperature_level,
-					  int16_t DELTA_UV,
-					  int retrans);
+void send_lighting_lightness(uint16_t level,
+		uint8_t kind,
+		uint8_t flag,
+		uint16_t transition,
+		uint16_t delay,
+		int retrans);
+
+#define SEND_LIGHT_DATA(level,flag) send_lighting_lightness(level,\
+									mesh_lighting_request_lightness_actual,\
+									flag,0,0,0)
+
+#define SEND_WATER_DATA(level,flag) send_lighting_lightness(level,\
+									mesh_lighting_request_lightness_linear,\
+									flag,0,0,0)
 
 void send_sensor_data_ctl(uint16_t lightness_level,
 		uint16_t temperature_level,int16_t DELTA_UV,int retrans);
