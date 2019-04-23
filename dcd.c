dd2fb2ae8d737bd9a5919217f52296d28b56b200
aakash <Aakash.Kumar@colorado.edu>
2019-04-22 21:40:41 -0600

    Threw out Generic Power On Off Server model, pub sub with on off model works, but lightness model pub sub stopped working. Does not work even after reprovisioning.

--- dcd.c
+++ dcd.c
@@ -32,7 +32,7 @@ const uint8_t __mesh_dcd[] = {
     0x07, 0x00, /* Features Bitmask = 0x0007 */
     /* Begin Primary Element */
         0x00, 0x00, /* Location = 0x0000 */
-        0x08, /* Number of SIG Models = 0x08 */
+        0x07, /* Number of SIG Models = 0x07 */
         0x00, /* Number of Vendor Models = 0x00 */
         /* Begin SIG Models */
         0x00, 0x00, /* Configuration Server */
@@ -40,7 +40,6 @@ const uint8_t __mesh_dcd[] = {
         0x00, 0x13, /* Light Lightness Server */
         0x01, 0x13, /* Light Lightness Setup Server */
         0x02, 0x10, /* Generic Level Server */
-        0x06, 0x10, /* Generic Power OnOff Server */
         0x03, 0x13, /* Light CTL Server */
         0x04, 0x13, /* Light CTL Setup Server */
         /* End SIG Models */
