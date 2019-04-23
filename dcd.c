806c4b3386c41db48101a20f886bad3f47831815
aakash <Aakash.Kumar@colorado.edu>
2019-04-22 21:22:21 -0600

    removed generic default transition time server, it changed only mesh_app_memory_config.h, dcd.c. Subscribe still working

--- dcd.c
+++ dcd.c
@@ -32,7 +32,7 @@ const uint8_t __mesh_dcd[] = {
     0x07, 0x00, /* Features Bitmask = 0x0007 */
     /* Begin Primary Element */
         0x00, 0x00, /* Location = 0x0000 */
-        0x0b, /* Number of SIG Models = 0x0b */
+        0x0a, /* Number of SIG Models = 0x0a */
         0x00, /* Number of Vendor Models = 0x00 */
         /* Begin SIG Models */
         0x00, 0x00, /* Configuration Server */
@@ -43,7 +43,6 @@ const uint8_t __mesh_dcd[] = {
         0x02, 0x10, /* Generic Level Server */
         0x06, 0x10, /* Generic Power OnOff Server */
         0x07, 0x10, /* Generic Power OnOff Setup Server */
-        0x04, 0x10, /* Generic Default Transition Time Server */
         0x03, 0x13, /* Light CTL Server */
         0x04, 0x13, /* Light CTL Setup Server */
         /* End SIG Models */
