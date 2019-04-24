10acfb817ead31635e57734ea3d010c8d908c76f
aakash <Aakash.Kumar@colorado.edu>
2019-04-24 13:04:09 -0600

    Added types for water and light sensor data

--- src/actuator_node_mesh.h
+++ src/actuator_node_mesh.h
@@ -10,22 +10,26 @@
 
 #include "gecko_helper.h"
 
+typedef enum {
+	full,
+	half,
+	error,
+	empty
+}water_level_t;
+
+
+typedef struct {
+	uint16_t light_level;
+	uint16_t light_setpoint;
+	uint16_t deadband;
+	uint8_t light_output;
+	uint8_t light_state;
+} light_data_t;
+
+
 void actuator_node_init(void);
 
-void onoff_request(uint16_t model_id,
-                          uint16_t element_index,
-                          uint16_t client_addr,
-                          uint16_t server_addr,
-                          uint16_t appkey_index,
-                          const struct mesh_generic_request *request,
-                          uint32_t transition_ms,
-                          uint16_t delay_ms,
-                          uint8_t request_flags);
 
-void onoff_change(uint16_t model_id,
-                         uint16_t element_index,
-                         const struct mesh_generic_state *current,
-                         const struct mesh_generic_state *target,
-                         uint32_t remaining_ms);
+
 
 #endif /* SRC_ACTUATOR_NODE_MESH_H_ */
