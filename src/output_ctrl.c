452c54d93e30c11d5c78441679873f439198fbb9
aakash <Aakash.Kumar@colorado.edu>
2019-04-24 21:55:01 -0600

    Added Pump control functions Flash write and read functions

--- src/output_ctrl.c
+++ src/output_ctrl.c
@@ -7,6 +7,113 @@
 
 #include "output_ctrl.h"
 
+// Structures for sensor value and actuator state
+static light_data_t light_val;
+static water_data_t water_val;
+static changed_light_setpoint;
+
+
+int light_actuator_state_load(void)
+{
+  struct gecko_msg_flash_ps_load_rsp_t* pLoad;
+
+  pLoad = gecko_cmd_flash_ps_load(LIGHT_STATE_KEY);
+
+  // Set default values if ps_load fail
+  if (pLoad->result ) {
+    memset(&light_val.actuator, 0, sizeof(light_actuator_t));
+    light_val.actuator.light_setpoint = DEFAULT_SETPOINT;
+    light_val.actuator.deadband = DEFAULT_DEADBAND;
+	light_val.actuator.light_output = 0;
+    return -1;
+  }
+
+  memcpy(&light_val.actuator, pLoad->value.data, pLoad->value.len);
+
+  return 0;
+}
+
+/***************************************************************************//**
+ * This function saves the current light state in Persistent Storage so that
+ * the data is preserved over reboots and power cycles.
+ * The light state is hold in a global variable lightbulb_state.
+ * A PS key with ID 0x4008 is used to store the whole struct.
+ *
+ * @return 0 if saving succeed, -1 if saving fails.
+ ******************************************************************************/
+int light_actuator_state_store(void)
+{
+  struct gecko_msg_flash_ps_save_rsp_t* pSave;
+
+  pSave = gecko_cmd_flash_ps_save(LIGHT_STATE_KEY,
+		  sizeof(light_actuator_t),
+		  (const uint8*)&light_val.actuator);
+
+  if (pSave->result) {
+    LOG_ERROR("lightbulb_state_store(): PS save failed, code %x", pSave->result);
+    return(-1);
+  }
+
+  return 0;
+}
+
+
+void update_light_state(void)
+{
+	light_val.sensor.light_level;
+	//calc_new_output
+	// check state changed
+		//actuator change
+		//update state in NVM
+}
+
+void update_pump_state(void)
+{
+	uint8_t pump_state;
+
+	switch(water_val.water_level)
+	{
+		case full:
+			pump_state = 0;
+			break;
+		case half:
+			pump_state = 0;
+		break;
+		case empty:
+			pump_state = 1;
+			break;
+		default: // in case of error, turn pump off
+			pump_state = 0;
+		break;
+	}
+
+	if(pump_state != water_val.pump_state)
+	{
+		//Update pump state
+		water_val.pump_state = pump_state;
+		//Turn pump on or off
+		water_val.pump_state ? pump_on():pump_off();
+
+		//TODO: Write new pump state to flash
+	}
+	LOG_INFO("---Water level %d Pump State %d",
+			water_val.water_level,
+			water_val.pump_state);
+}
+
+void update_light_setpoint(void)
+{
+	LOG_INFO("New Light Setpoint %d",changed_light_setpoint);
+
+	if(changed_light_setpoint != light_val.actuator.light_setpoint)
+	{
+		light_val.actuator.light_setpoint = changed_light_setpoint;
+		// write value to flash here
+		light_actuator_state_store();
+	}
+}
+
+
 void pump_control_init()
 {
 	GPIO_DriveStrengthSet(PUMP_PORT, gpioDriveStrengthWeakAlternateWeak);
@@ -16,9 +123,6 @@ void pump_control_init()
 	 * Read last value from NVM and
 	 * set the pump to that value on restart
 	 */
-	//gpioLed0SetOff();
-	//gpioLed1SetOff();
-
 }
 
 
@@ -29,17 +133,40 @@ void pump_control_init()
  */
 void pump_on()
 {
+	/*
+	 * Start a timer here,
+	 * turn off pump when the timer expires
+	 * in case the sensor is broken
+	 */
 	GPIO_PinOutSet(PUMP_PORT,PUMP_PIN);
 	LOG_INFO("Pump turned ON");
+	displayPrintf(PUMP_ACTION,"PUMP ON");
 }
 
 void pump_off()
 {
 	GPIO_PinOutClear(PUMP_PORT,PUMP_PIN);
 	LOG_INFO("Pump turned OFF");
+	displayPrintf(PUMP_ACTION,"PUMP OFF");
 }
 
-bool light_output_calc()
+void set_light_val(uint16_t val,bool sensor_state)
+{
+	light_val.sensor.light_level = val;
+	light_val.sensor.sensor_state = sensor_state;
+}
+
+void set_water_level(uint16_t val)
+{
+	water_val.water_level = val;
+}
+
+void set_changed_light_setpoint(uint16_t val)
+{
+	changed_light_setpoint = val;
+}
+
+bool light_output_calc(uint16_t light_val)
 {
 
 }
--- src/output_ctrl.h
+++ src/output_ctrl.h
@@ -11,23 +11,66 @@
 #include <stdbool.h>
 #include "em_gpio.h"
 #include "log.h"
+#include "display.h"
 
 #define PUMP_PORT gpioPortF
-#define PUMP_PIN (6U)
+#define PUMP_PIN (5U)
 #define LIGHT_PORT gpioPortF
 #define LIGHT_PIN (7U)
 
-#define LIGHT_IN_MIN 10
-#define LIGHT_IN_MAX 50
+#define LIGHT_IN_MIN 0
+#define LIGHT_IN_MAX 300
 #define LIGHT_OUT_MIN 0
 #define LIGHT_OUT_MAX 100
 
+#define DEFAULT_SETPOINT 150
+#define DEFAULT_DEADBAND 10
+
+#define LIGHT_STATE_KEY 0x4008
+#define PUMP_STATE_KEY 0x4009
+
+typedef enum {
+	undefined,
+	full,
+	error,
+	half,
+	empty
+}water_level_t;
+
+
+typedef struct{
+	uint16_t light_setpoint;
+	uint16_t deadband;
+	uint8_t light_output;
+} light_actuator_t;
+
+typedef struct {
+	uint16_t light_level;
+	bool sensor_state; //true is sensor OK,false is sensor error
+} light_sensor_t;
+
+typedef struct {
+	light_sensor_t sensor;
+	light_actuator_t actuator;
+}light_data_t;
+
+typedef struct{
+	water_level_t water_level;
+	uint8_t pump_state;
+}water_data_t;
+
+void set_light_val(uint16_t val,bool sensor_state);
+void set_water_level(uint16_t val);
+void set_changed_light_setpoint(uint16_t val);
 
 void pump_control_init();
 void light_control_init();
 
-void pump_on();
+void update_light_state(void);
+void update_pump_state(void);
+void update_light_setpoint(void);
 
+void pump_on();
 void pump_off();
 
 void light_level_set();
