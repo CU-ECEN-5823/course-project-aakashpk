a24bef1e5e665cd95981874162872ba5c93e6de9
aakash <Aakash.Kumar@colorado.edu>
2019-04-25 03:36:37 -0600

    Changes to help testing

--- src/events.c
+++ src/events.c
@@ -35,15 +35,17 @@ void button_press(uint8 flags)
 void log_temp_task(void)
 {
 	static uint8_t state = 0, water = 1;
-	int32_t temp;
-	uint32_t humidity;
-	get_temp_humidity(&temp, &humidity);
-	LOG_INFO("Temp %f Water %d State %x",temp/1000.0,water,state);
+	static uint16_t light = 30;
 
-	SEND_LIGHT_DATA(temp,0);
-	SEND_WATER_DATA(water,3);
+	SEND_LIGHT_DATA(light,1);
+	SEND_WATER_DATA(water,0);
+	LOG_INFO("Light:%d Water:%d %s",
+			light,water,
+			state?"ON":"OFF");
 	water++;
+	light+=22;
 	if(water>4) water =1;
+	if(light > 300) light = 30;
 //	send_button_state(state);
 	state = state ^ 0x01;
 }
@@ -66,7 +68,6 @@ void light_actuator_task(void)
 void pump_actuator_task(void)
 {
 	update_pump_state();
-
 }
 
 //Task 2
