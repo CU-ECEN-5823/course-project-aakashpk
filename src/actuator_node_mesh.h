a86697ec5e6023e004abd4ec09db677e0ddb4390
aakash <Aakash.Kumar@colorado.edu>
2019-04-28 02:55:11 -0600

    Changed task number to not mess with sensor test code

--- src/actuator_node_mesh.h
+++ src/actuator_node_mesh.h
@@ -12,11 +12,11 @@
 #include "scheduler.h"
 #include "output_ctrl.h"
 
-#define LIGHT_TASK Task0
-#define WATER_TASK Task1
-#define SETPOINT_CHANGE_TASK Task2
-#define DEADBAND_CHANGE_TASK Task3
-#define CONFIG_CHANGE_TASK Task4
+#define LIGHT_TASK Task2
+#define WATER_TASK Task3
+#define SETPOINT_CHANGE_TASK Task4
+#define DEADBAND_CHANGE_TASK Task5
+#define CONFIG_CHANGE_TASK Task6
 
 void actuator_node_init(void);
 
